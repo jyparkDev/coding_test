8173ef0c765d07f1785a34b758aacc18424213ba
Parkjaeyong <pjjy92@gmail.com>
2023-01-20 16:35:23 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int service = n / 10;
+        k -= service;
+        
+        return (n*12000)+(k*2000);
+    }
+}
