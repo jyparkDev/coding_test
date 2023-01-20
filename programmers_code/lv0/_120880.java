34b48b6c4e09719b41cd207c0e16eafc8a7359a8
Parkjaeyong <pjjy92@gmail.com>
2023-01-20 12:27:36 +0900

    feat : 프로그래머스 LV0 120853,120880,120923 풀이

---
+++ programmers_code/lv0/_120880.java
@@ -0,0 +1,4 @@
+package programmers.lv0;
+
+public class _120880 {
+}
---
+++ programmers_code/lv0/_120923.java
@@ -0,0 +1,34 @@
+package programmers.lv0;
+
+import java.util.Arrays;
+
+public class _120923 {
+    public static int[] solution(int num, int total) {
+        int[] answer ;
+        int dp=1 , upp = num;
+        int sum_status = 0;
+        for(int i = 1 ; i <= num ; i++){
+            sum_status += i;
+        }
+        while (sum_status != total){
+            if(sum_status < total){
+                sum_status -= dp++;
+                sum_status += ++upp;
+            }else{
+                sum_status -= upp--;
+                sum_status += --dp;
+            }
+        }
+        answer = new int[num];
+        for(int i = dp ,k=0 ; i <= upp ; i++){
+            answer[k++] = i;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+        int result[] = solution(5, 5);
+        System.out.println(Arrays.toString(result));
+    }
+}
