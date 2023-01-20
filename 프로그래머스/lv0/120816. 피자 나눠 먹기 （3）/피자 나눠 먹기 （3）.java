53eb08d25d5ff1eed0721e65cecefe0729e48b07
Parkjaeyong <pjjy92@gmail.com>
2023-01-20 21:39:43 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int slice, int n) {
         int answer ;
         for(answer=0 ; (answer * slice) < n ; answer++ );
-            
         return answer;
     }
 }
