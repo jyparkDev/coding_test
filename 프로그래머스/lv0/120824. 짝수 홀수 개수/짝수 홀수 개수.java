d4774dd53b5869cefbda8919464a5b8cc681056d
Parkjaeyong <pjjy92@gmail.com>
2023-01-23 07:13:08 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 2.15 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
-        // 테스트 결과 반복문을 통하여 처리할 경우가 속도에서 꽤나 차이가 발생했다.
-        for(int i = 0 ; i < num_list.length ; i++)
-            answer[num_list[i] % 2]++;
-//        answer[0] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
-//        answer[1] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 1).count();
-        return answer;
+        // int[] answer = new int[2];
+        // for(int i = 0 ; i < num_list.length ; i++){
+        //     answer[num_list[i] % 2]++;
+        // }
+        // answer[0] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
+        // answer[1] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 1).count();
+        return IntStream
+                .of((int) Arrays.stream(num_list).filter(i -> i % 2 == 0).count(),(int) Arrays.stream(num_list).filter(i -> i % 2 != 0).count())
+                .toArray();
     }
 }
