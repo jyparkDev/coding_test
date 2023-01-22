c1b9b39a3103a2487f005c35678252e5db2929a4
Parkjaeyong <pjjy92@gmail.com>
2023-01-23 04:54:00 +0900

    fix : 문제 코드 성능 개선

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
-        answer[0] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
-        answer[1] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 1).count();
+        // 테스트 결과 반복문을 통하여 처리할 경우가 속도에서 꽤나 차이가 발생했다.
+        for(int i = 0 ; i < num_list.length ; i++)
+            answer[num_list[i] % 2]++;
+//        answer[0] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
+//        answer[1] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 1).count();
         return answer;
     }
 }
