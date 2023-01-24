307d31e7f079f7f500d73964dffc640e828d5599
Parkjaeyong <pjjy92@gmail.com>
2023-01-24 17:46:28 +0900

    fix : 120824 프로그래머스 성능 테스트

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] num_list) {
+
         int[] answer = new int[2];
         // 테스트 결과 반복문을 통하여 처리할 경우가 속도에서 꽤나 차이가 발생했다.
         for(int i = 0 ; i < num_list.length ; i++)
@@ -8,5 +11,6 @@ class Solution {
 //        answer[0] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 0).count();
 //        answer[1] = (int)Arrays.stream(num_list).filter(i-> i % 2 == 1).count();
         return answer;
+
     }
 }
