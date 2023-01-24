10b7f981ebd5202bb419fd4dda37022aa606eaea
Parkjaeyong <pjjy92@gmail.com>
2023-01-24 15:25:09 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.47 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -4,10 +4,9 @@ class Solution {
     public int solution(int[] array, int height) {
         Arrays.sort(array);
         int answer = 0;
-        for(int i = array.length -1 ; i >= 0 ; i--){
+
+        for(int i = array.length -1 ; i >= 0 ; i--, answer++)
             if(array[i] <= height) break;
-            else ++answer;
-        }
         return answer;
     }
 }
