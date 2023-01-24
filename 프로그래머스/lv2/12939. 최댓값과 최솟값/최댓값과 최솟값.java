ad9e6a752bd3bfc862cf3d84d72d7c868b198867
Parkjaeyong <pjjy92@gmail.com>
2023-01-24 19:17:44 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 18.46 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+         String[] strArr = s.split(" ");
+        int[] result =Arrays.stream(strArr).mapToInt( x  -> Integer.parseInt(x)).distinct().sorted().toArray();
+        String answer = result[0] + " " + result[result.length-1];
+        return answer;
+    }
+}
