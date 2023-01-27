95697b4a6a7290ff59feb36c6e30e77b5de3ef13
Parkjaeyong <pjjy92@gmail.com>
2023-01-27 12:29:30 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
+        char[] ch = String.valueOf(n).toCharArray();
         int answer = 0;
-        while(n > 0){
-            answer += n%10;
-            n /=10;
-        }
+        for(int i = 0 ; i <  ch.length ; i++)
+            answer  += ch[i] - '0';
         return answer;
     }
 }
