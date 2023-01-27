8206801b9c364d2227d18c864c2f071845f89fab
Parkjaeyong <pjjy92@gmail.com>
2023-01-27 12:13:18 +0900

    fix : 프로그래머스 LV1 12922 성능 향상 추가

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,8 +1,17 @@
 class Solution {
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
-        if(n%2==0) sb.append("수박".repeat(n/2));
+        //깔끔한 코드 만들기
+        /* if(n%2==0) sb.append("수박".repeat(n/2));
         else sb.append("수박".repeat(n/2)+"수");
+        */
+
+        //성능향상시키기
+        char[] sample = "수박".toCharArray();
+        for(int i =  0; i < n ; i++){
+            sb.append(sample[i%2]);
+        }
+
         return sb.toString();
     }
 }
