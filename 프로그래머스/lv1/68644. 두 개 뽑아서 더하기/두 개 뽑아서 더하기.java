c8648b2fc93ad51f4aab358ea7257e6d6daff880
Parkjaeyong <pjjy92@gmail.com>
2023-01-29 08:23:16 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.34 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] check = new int[201];
+        int max = 0;
+        int num = 0;
+        for(int i = 0 ; i < numbers.length - 1 ; i++){
+            for(int j = i+1 ; j < numbers.length ; j++){
+                int n = numbers[i]+numbers[j];
+                check[n]++;
+                if(max <= n) max = n;
+                if(check[n] == 1) num++;
+            }
+        }
+        int answer[] = new int[num];
+        for(int i = 0 , k = 0; i <= max ; i++)
+            if(check[i] > 0){
+                answer[k++] = i;
+            }
+        return answer;
+    }
+}
