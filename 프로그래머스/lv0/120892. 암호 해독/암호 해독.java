dea49da02d48985863427bfd2f0329c39a8d8d3c
Parkjaeyong <pjjy92@gmail.com>
2023-01-29 00:01:12 +0900

    [level 0] Title: 암호 해독, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -2,8 +2,8 @@ class Solution {
     public String solution(String cipher, int code) {
         int n = 0;
         StringBuilder sb = new StringBuilder();
-        while((++n * code-1) < cipher.length())
-            sb.append(cipher.charAt(n*code-1));
+        while((++n * code -1) < cipher.length())
+            sb.append(cipher.charAt(n * code -1));
         return sb.toString();
     }
 }
