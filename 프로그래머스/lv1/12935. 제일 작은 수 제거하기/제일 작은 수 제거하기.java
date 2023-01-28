b58335e50888a923549ed2bff63a4bf00610d2a1
Parkjaeyong <pjjy92@gmail.com>
2023-01-28 10:42:40 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.65 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+         if(arr.length == 1){
+            return new int[]{-1};
+        }
+        int[] answer = arr.clone();
+        Arrays.sort(arr);
+        int min = arr[0];
+        arr = new int[answer.length-1];
+        for(int i = 0 ; i < answer.length;i++){
+            if(answer[i] == min){
+                for(int j = i+1; j < answer.length; j++){
+                    arr[j-1] = answer[j];
+                }
+                return arr;
+            }
+            else arr[i] = answer[i];
+        }
+        return arr;
+    }
+}
