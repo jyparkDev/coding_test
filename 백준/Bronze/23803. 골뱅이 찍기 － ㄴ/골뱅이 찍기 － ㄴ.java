8fd924fb63b3f61ea1f26c6b41d827913a7c8715
Parkjaeyong <pjjy92@gmail.com>
2023-02-11 03:22:45 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 236 ms, Memory: 18012 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            System.out.println("@".repeat(n));
+            System.out.println("@".repeat(n));
+            System.out.println("@".repeat(n));
+            System.out.println("@".repeat(n));
+        }
+        for(int i = 0 ; i < n ; i++){
+            System.out.println("@".repeat(5*n));
+        }
+        
+    }
+}
