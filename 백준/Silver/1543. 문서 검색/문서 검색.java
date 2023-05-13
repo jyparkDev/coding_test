61a9543ae72ef18c350d05030455db69f985c57d
Parkjaeyong <pjjy92@gmail.com>
2023-05-14 02:00:39 +0900

    [Silver IV] Title: 문서 검색, Time: 224 ms, Memory: 17772 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,30 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String target = br.readLine();
-        String word = br.readLine();
-        int targetLength = target.length();
-        int wordLength = word.length();
-        int num = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
 
-        for( int i = 0 ; i <= (targetLength - wordLength) ; ){
-            for (int j = 0, count = 0 ; j < wordLength; j++){
-                if(target.charAt(i + j) == word.charAt(j)){
-                    ++count;
-                    if(count == wordLength) {
-                        ++num;
-                        i += (j + 1);
-                    }
-                    continue;
-                }
-                i++;
-                break;
-            }
-        }
-        System.out.println(num);
+        String replaced = doc.replace(word, "");
+        int length = doc.length() - replaced.length();
+        int count = length / word.length();
+
+        System.out.println(count);
     }
 }
