e2217a33a01ab1caab01afe537152f9969ea1c72
Parkjaeyong <pjjy92@gmail.com>
2023-05-19 13:13:43 +0900

    [Silver V] Title: 단어 정렬, Time: 932 ms, Memory: 39708 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,21 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
-class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        String[] str = new String[n];
-        for(int i = 0 ; i < n ; i++){
-            str[i] = s.next();
-        }
-        Arrays.stream(str).sorted( new Comparator<String>() {
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] str = new String[N];
+
+        for(int i = 0 ; i < N ; i++)
+            str[i] = sc.next();
+
+        Arrays.sort(str, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length() != o2.length()){
-                    return o1.length() - o2.length();
-                }
-                return o1.compareTo(o2);
+                if(o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                return o1.length() - o2.length();
             }
-        }).distinct().forEach(i -> System.out.println(i));
+        });
+
+        System.out.println(str[0]);
+        for(int i = 1 ; i < N ; i++){
+            if (!str[i-1].equals(str[i])){
+                System.out.println(str[i]);
+            }
+        }
     }
 }
+
