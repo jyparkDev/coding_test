74711dbae421e6d3fe90b3a3ae2460d1c0283e08
Parkjaeyong <pjjy92@gmail.com>
2023-05-30 23:22:37 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        char[] word = scan.next().toCharArray();
+
+        boolean isOk = false;
+        for(int i = 0 ; i <= word.length / 2 ; i++){
+            if(word[i] != word[word.length - i - 1]){
+                isOk = true;
+                 break;
+            }
+        }
+        System.out.println(isOk ? 0 : 1);
+
+    }
+
+
+
+    static class FastReader{
+        BufferedReader br ;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next(){
+            while(st == null || !st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return str;
+        }
+    }
+}
