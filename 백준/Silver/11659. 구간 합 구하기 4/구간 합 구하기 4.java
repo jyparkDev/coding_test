273562f3fbdb7d8c35211b395e9e61b5d82ba950
Parkjaeyong <pjjy92@gmail.com>
2023-06-19 19:02:43 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 620 ms, Memory: 48912 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,20 +1,82 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+
+    static int N, M;
+    static int[] S;
+
+    public static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        S = new int[N + 1];
+        for(int i = 1 ; i <= N ; i++){
+            int num = scan.nextInt();
+            S[i] += S[i - 1] + num;
+        }
+    }
+    public static void pro() {
+        while (M-- > 0){
+            int i = scan.nextInt();
+            int j = scan.nextInt();
+            sb.append(S[j] - S[i - 1]);
+            sb.append('\n');
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        input();
+        pro();
+        System.out.println(sb.toString());
+    }
 
-        int[] acc = new int[N + 1];
+    static class FastReader{
+        BufferedReader br ;
+        StringTokenizer st;
 
-        for(int i = 1 ; i < acc.length ; i++)
-            acc[i] = acc[ i - 1 ] + sc.nextInt();
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-        while(M-- > 0){
-            int i = sc.nextInt();
-            int j = sc.nextInt();
-            System.out.println(acc[j] - acc[i - 1 ]);
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next(){
+            while(st == null || !st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return str;
         }
     }
 }
