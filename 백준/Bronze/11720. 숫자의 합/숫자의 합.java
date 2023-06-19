664888b1db9f0d9702d97f4bd3f2ee2d0f779461
Parkjaeyong <pjjy92@gmail.com>
2023-06-19 18:29:04 +0900

    [Bronze IV] Title: 숫자의 합, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,14 +1,76 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main{
-    public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        s.nextLine();
-        char[] num = s.nextLine().toCharArray();
+public class Main {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+
+    static int N;
+    static String M;
+
+    public static void input() {
+        N = scan.nextInt();
+        M = scan.nextLine();
+    }
+    public static void pro() {
+        char[] nums = M.toCharArray();
         int sum = 0;
-        for(int i = 0 ; i < num.length; i++){
-            sum += num[i] - '0';
+        for (char n : nums) {
+            sum += n - '0';
         }
         System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader{
+        BufferedReader br ;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next(){
+            while(st == null || !st.hasMoreElements()){
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return str;
+        }
     }
 }
