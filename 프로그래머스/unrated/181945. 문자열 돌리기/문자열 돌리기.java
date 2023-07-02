47c962babcec19e82f7134ae854701b5a531810c
Parkjaeyong <pjjy92@gmail.com>
2023-07-03 01:54:54 +0900

    [unrated] Title: 문자열 돌리기, Time: 187.93 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        char[] ch = sc.next().toCharArray();
+        for(char c : ch){
+            System.out.println(c);
+        }
+    }
+}
