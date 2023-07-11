2b37d6bb3da91afbd0bb0a24a85ece2d3990313d
Parkjaeyong <pjjy92@gmail.com>
2023-07-11 20:45:22 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        if(a % 2 == 1 && b % 2 == 1){
+            return (a * a) + (b * b);
+        }else if(a % 2 == 1 || b % 2 == 1){
+            return 2 * ( a + b );
+        }
+        return Math.abs(a - b);
+    }
+}
