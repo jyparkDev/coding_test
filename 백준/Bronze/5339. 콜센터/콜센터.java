f5ad2c3350ae62dfe789740e61b4780b40d0798a
Parkjaeyong <pjjy92@gmail.com>
2023-10-26 22:46:34 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
