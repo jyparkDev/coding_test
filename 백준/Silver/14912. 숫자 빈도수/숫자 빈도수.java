dd3a90bb78c74050a0c2acb7b3ed0dec4abc79b4
Parkjaeyong <pjjy92@gmail.com>
2024-01-12 22:25:05 +0900

    [Silver V] Title: 숫자 빈도수, Time: 428 ms, Memory: 45268 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -1,9 +1,7 @@
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -11,14 +9,13 @@ public class Main {
         String[] str = br.readLine().split(" ");
         int N = Integer.parseInt(str[0]);
         String d = str[1];
+        StringBuilder sb = new StringBuilder();
 
         int count = 0 ;
 
         for(int i = 1 ; i <= N ; i++){
-            count += String.valueOf(i)
-                    .replaceAll("[^"+d+"]","")
-                    .length();
+            sb.append(i);
         }
-        System.out.println(count);
+        System.out.println(sb.toString().replaceAll("[^"+d+"]","").length());
     }
 }
