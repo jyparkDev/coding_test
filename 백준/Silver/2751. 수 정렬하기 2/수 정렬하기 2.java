d6c61074c4c4cb9c8d95d4d46020f52538dd6faa
Parkjaeyong <pjjy92@gmail.com>
2024-03-06 23:39:11 +0900

    docs : 풀이 수정
    issue : 실제 boj 풀이와 다른 풀이 commit

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.io.*;
 
+
 class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,5 +17,37 @@ class Main{
             if(i != arr.length - 1) sb.append("\n");
         }
         System.out.print(sb.toString());
+    }
+}
+
+/* sol */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class _2751 {
+    public static void main(String[] args) throws IOException {
+
+        System.setIn(new FileInputStream("src/input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (Integer i : list) {
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
